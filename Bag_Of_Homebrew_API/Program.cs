006a6e4e96ee2a4e0bc463847bff5a2055e85681
Conall Mone <conallmone1@gmail.com>
2026-08-17 13:35:10 +0100

    database

--- Bag_Of_Homebrew_API/Program.cs
+++ Bag_Of_Homebrew_API/Program.cs
@@ -56,6 +56,12 @@ if (builder.Environment.IsDevelopment())
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseCors("ReactApp");
